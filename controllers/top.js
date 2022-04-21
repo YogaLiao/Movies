c19683bfc0435248c3f0686e3f0eb722746ae05c
YogaLiao <liaoyujia0702@gmail.com>
2022-04-20 22:00:03 -0500

    CRUD Done

--- controllers/top.js
+++ controllers/top.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Top = require("../models/top");
+const axios = require("axios")
 const router = express.Router()
 
 router.get("/", (req, res) => {
@@ -20,10 +21,46 @@ router.get("/", (req, res) => {
         .catch(console.error)
 })
 
+router.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+router.post("/add", (req, res) => {
+    Top.create(req.body)
+        .then(() => {
+        res.redirect("/movies")
+    })
+})
+
+router.get("/:id/edit", (req, res) => {
+    Top.findById(req.params.id)
+        .then((data) => {
+            res.render("edit.ejs", { data })
+            
+        })
+        .catch(console.error)
+})
+
+router.put("/:id", (req, res) => {
+    Top.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(() => res.redirect("/movies"))
+})
+
 router.get("/:id", (req, res) => {
     Top.findById(req.params.id)
-        .then((data) => res.render("show", { data }))
+        .then(async (data) => {
+            console.log(data.id)
+            const cast = await axios.get(`https://imdb-api.com/en/API/FullCast/k_1fn0eocd/${data.id}`);
+            const description = await axios.get(`https://imdb-api.com/en/API/Wikipedia/k_1fn0eocd/${data.id}`);
+            const posterInfo = await axios.get(`https://imdb-api.com/en/API/Posters/k_1fn0eocd/${data.id}`)
+            res.render("show", {data: data, cast: cast.data, wiki: description.data, poster: posterInfo.data})
+        })
         .catch(console.error)
+})
+
+router.delete("/:id", (req, res) => {
+    Top.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect("/movies"))
 })
 
 
