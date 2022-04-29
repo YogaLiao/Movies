770816ddd5f3d93e3c707b9f8a89385aa30b1e5c
YogaLiao <liaoyujia0702@gmail.com>
2022-04-29 17:27:29 -0500

    #6

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ app.set("view engine", "ejs")
 app.use(methodOverride("_method"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/favicon.ico', (req, res) => {res.send("dummy")})
-app.use("/movies",topController)
-
-const port = process.env.port || 4000
+app.get('/favicon.ico', (req, res) => { res.send("dummy") })
 
 app.get("/", (req, res) => {
     res.render("home")
 })
+
+app.use("/movies",topController)
+
+const port = process.env.PORT || 4000
+
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
